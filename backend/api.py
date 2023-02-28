c05eb2e08c45c775e52c41d984fc27ce4a2f1b7a
bradpickering <bradleepickering@gmail.com>
2023-02-28 14:25:20 -0800

    flask template created

--- backend/api.py
+++ backend/api.py
@@ -1,7 +1,50 @@
-from flask import Flask
-
+from flask import Flask, requests
+# import mongodb
 app = Flask(__name__)
 
+@app.route("/quote", methods=["GET"])
+def get_quote():
+    pass
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    pass
+
+
+@app.route("/display_summary", methods=["GET"])
+def display_summary():
+    pass
+
+
+@app.route("/buy", methods=["POST"])
+def buy():
+    pass
+
+
+@app.route("/commit_buy", methods=["POST"])
+def commit_buy():
+    pass
+
+
+@app.route("/cancel_buy", methods=["POST"])
+def cancel_buy():
+    pass
+
+
+@app.route("/sell", methods=["POST"])
+def sell():
+    pass
+
+
+@app.route("/commit_sell", methods=["POST"])
+def commit_sell():
+    pass
+
+
+@app.route("/cancel_sell", methods=["POST"])
+def cancel_sell():
+    pass
 
 
 
