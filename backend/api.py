d8aaa07dc29f2a0ef8e194bfc5e9ca1f1e566d0c
Vijaivir Dhaliwal <vijaivirdhaliwal@gmail.com>
2023-02-28 20:34:57 -0800

    some comments

--- backend/api.py
+++ backend/api.py
@@ -17,6 +17,7 @@ def get_quote():
     return price
 
 
+
 @app.route("/add", methods=["POST"])
 def add():
     # recive data from API
@@ -40,18 +41,19 @@ def add():
     # else, update existing user
     else:
         balance = collection.find_one(filter)["funds"]
-        update_funds = {"$set": {"funds": float(balance) + float(data['amount'])},
+        update = {"$set": {"funds": float(balance) + float(data['amount'])},
                         "$push": {"transactions": {
                             "timestamp":time.time(),
                             "server":"TS1",
                             "transactionNum":data['trxNum'],
                             "command":data['cmd']
                         }}}
-        collection.update_one(filter, update_funds)
+        collection.update_one(filter, update)
 
-    for d in collection.find():
-        print(d)
-    return data
+    # for d in collection.find():
+    #     print(d)
+    # return data
+
 
 
 @app.route("/display_summary", methods=["POST"])
@@ -83,28 +85,52 @@ def buy():
 
 @app.route("/commit_buy", methods=["POST"])
 def commit_buy():
+    # check if a BUY command was executed in the last 60 seconds
+
+    # if yes,
+    # i) decrease funds from user
+    # ii) increase stock price for symbol (or add a new stock to "stocks":[{}])
+    # remove pending flag
 
 
+    # else, return error
     pass
 
 
 @app.route("/cancel_buy", methods=["POST"])
 def cancel_buy():
+    # check if a BUY command was executed in the last 60 seconds
+
+    # if yes, cancel the transaction (remove pending flag)
+    # dont add to stocks[]
     pass
 
 
 @app.route("/sell", methods=["POST"])
 def sell():
-    pass
+    # check if owned stock is >= amount being sold
+    data = request.json
+
+    # if so, call commit_sell() or cancel_sell()
+    return data
 
 
 @app.route("/commit_sell", methods=["POST"])
 def commit_sell():
+    # check if a SELL command was executed in the last 60 seconds
+
+    # if yes,
+    # i) decrease funds from user stock
+    # ii) increase user funds
     pass
 
 
 @app.route("/cancel_sell", methods=["POST"])
 def cancel_sell():
+    # check if a SELL command was executed in the last 60 seconds
+
+    # if yes, cancel the transaction (remove pending flag)
+    # dont remove from stocks[]  
     pass
 
 
