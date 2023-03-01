3cee39c32d627eb874cd64685a497c9e89a84bb7
bradpickering <bradleepickering@gmail.com>
2023-02-28 22:31:02 -0800

    commit buy half working, you can buy the same stock as many times as you want

--- backend/api.py
+++ backend/api.py
@@ -64,7 +64,7 @@ def buy():
     data = request.json
     filter = {"username":data['username']}
 
-    new_buy = {"$push": { "transactions": {
+    new_tx = {"$push": { "transactions": {
             "timestamp":time.time(),
             "server":"TS1",
             "transactionNum":data['trxNum'],
@@ -75,7 +75,7 @@ def buy():
         }
     }
     # update the array of transactions to include a buy
-    collection.update_one(filter, new_buy)
+    collection.update_one(filter, new_tx)
     for d in collection.find():
         print(d)
     return data
@@ -83,9 +83,80 @@ def buy():
 
 @app.route("/commit_buy", methods=["POST"])
 def commit_buy():
+    data = request.json
+    timestamp = time.time()
+    greater_than_time = timestamp - 60
 
+    # filter to get only commands of type BUY in the correct time
+    filter = {
+        "username": data["username"],
+        "transactions": {
+            "$elemMatch": {
+            "command": "BUY",
+            "timestamp": {
+                "$gte": greater_than_time
+                }
+            }
+        }
+    }
 
-    pass
+    # create the projection to extract only the price field from the matching transaction
+    fields = {
+        "_id": 0,
+        "price": {
+            "$filter": {
+            "input": "$transactions",
+            "as": "transaction",
+            "cond": {
+                "$and": [
+                { "$eq": [ "$$transaction.command", "BUY" ] },
+                { "$gte": [ "$$transaction.timestamp", greater_than_time ] }
+                ]
+            }
+            }
+        }
+    }
+
+    # perform the aggregation pipeline to match and extract the desired fields
+    valid_buy = list(collection.aggregate([
+            { "$match": filter },
+            { "$project": fields }
+        ]
+    ))
+
+    if len(valid_buy) > 0:
+        # subtract the funds and add the stock to user's stocks and add new transaction
+        valid_buy = list(valid_buy)[0]
+        buy_price = valid_buy["price"][0]["price"]
+        stock_bought = valid_buy["price"][0]["sym"]
+
+        balance = collection.find_one({"username":data["username"]})["funds"]
+        update_funds = {"$set": {"funds": float(balance) - buy_price},
+                        "$push": {"stocks":stock_bought},               
+                        "$push": {"transactions": {
+                            "timestamp":timestamp,
+                            "server":"TS1",
+                            "transactionNum":data['trxNum'],
+                            "command":data['cmd']
+                        }}}
+        collection.update_one({"username":data["username"]}, update_funds)
+
+        return "Successfly bought stock"
+
+    # add just a new transaction
+    new_tx = {"$push": { "transactions": {
+            "timestamp":timestamp,
+            "server":"TS1",
+            "transactionNum":data['trxNum'],
+            "command":data['cmd'],
+            }
+        }
+    }
+
+    collection.update_one({"username":data["username"]}, new_tx)
+    
+    return "No valid buys"
+
 
 
 @app.route("/cancel_buy", methods=["POST"])
@@ -108,6 +179,11 @@ def cancel_sell():
     pass
 
 
+@app.route("/clear", methods=["GET"])
+def clear():
+    collection.delete_many({})
+
+    return "cleared DB"
 
 if __name__ =="__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
