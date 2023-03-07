3e65377919c47346a6becac126e86de1f2e9d488
Davin <davcn6@gmail.com>
2023-03-07 15:44:11 -0800

    Started working on front-end react functionality

---
+++ frontend/src/TradingPage.js
@@ -0,0 +1,13 @@
+
+function navBar(props) {
+    return(
+        <div>
+            <div>
+                {props.username}
+            </div>
+        </div>
+    );
+
+}
+
+export default navBar;
