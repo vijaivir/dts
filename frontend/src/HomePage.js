79a84a6f02999ac3c7524ae06aa7eee579a0e324
bradpickering <bradleepickering@gmail.com>
2023-04-04 15:04:39 -0700

    giving props username to trading page and getting quote price is working

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -1,19 +1,29 @@
 import navBar from "./components/NavBar";
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import Login from "./LoginPage";
-import TradingPage from './TradingPage'
+import TradingPage from "./TradingPage";
 
 function HomePage() {
+  const [username, setUsername] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    const [username, setUsername] = useState('');
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-    return(
-        <div>
-            {isLoggedIn ? <><TradingPage username={username} isLoggedIn={isLoggedIn}></TradingPage></> : <Login setIsLoggedIn={setIsLoggedIn} setUsername={setUsername}></Login>}
-        </div>
-    );
-
+  useEffect(() => {
+    console.log(username);
+  }, [username]);
+  return (
+    <div>
+      {isLoggedIn ? (
+        <>
+          <TradingPage
+            username={username}
+            isLoggedIn={isLoggedIn}
+          ></TradingPage>
+        </>
+      ) : (
+        <Login setIsLoggedIn={setIsLoggedIn} setUsername={setUsername}></Login>
+      )}
+    </div>
+  );
 }
 
 export default HomePage;
